adf556927a70f4b0ec1761b09afc10e81aea7c53
ChenBaby <1911478180@qq.com>
2018-11-09 19:05:40 +0800

    vuex 共用user的信息，不放在module

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import modules from './modules'
+import user from './user.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    ...user,
     modules,
     "strict": process.env.NODE_ENV !== 'production'
 })
---
+++ src/renderer/store/user.js
@@ -0,0 +1,89 @@
+import ajax from '@/common/ajax.js'
+const state = {
+    "user": {},
+    "isLogged": false
+}
+
+const mutations = {
+    setUser (state, data) {
+        if (data && JSON.stringify(data) !== '{}') {
+            state.user = {...data}
+            localStorage.setItem('username', data.name)
+        } else {
+            state.user = {}
+            localStorage.setItem('username', '')
+        }
+    },
+    setIsLogged (state, booldata) {
+        if (booldata) {
+            state.isLogged = true
+            localStorage.setItem('isLogged', true)
+        } else {
+            state.isLogged = false
+            localStorage.setItem('isLogged', false)
+        }
+    },
+    setUserImage (state, url) {
+        state.user.userimage = url
+    },
+    setCK (state, data) {
+        localStorage.setItem('ck', data)
+    }
+}
+
+const actions = {
+    getUserInfo () {
+        return ajax.get('/chat/user/')
+    },
+    saveUserInfo (store, data) {
+        return ajax.post('/chat/user/save_info', data).then(res => {
+            if (res.success) {
+                console.log(res.data.message)
+                store.commit('setUser', {
+                    ...res.data.userInfo
+                })
+            }
+            return res
+        }).catch(err => {
+            return err
+        })
+    },
+    signIn ({commit}, data) {
+        return ajax.post('/chat/user/sign_in', data).then((res) => {
+            if (res.success) {
+                console.log(res.data.message)
+                commit('setUser', {
+                    ...res.data.userInfo
+                })
+                commit('setCK', res.data.ck)
+            }
+            return res
+        })
+    },
+    signUp (store, data) {
+        return ajax.post('/chat/user/sign_up', data)
+    },
+    signOut ({commit}) {
+        return ajax.post('/chat/user/sign_out')
+            .then((res) => {
+                if (res.success) {
+                    commit('setUser', null)
+                    commit('setIsLogged', false)
+                    commit('setCK', '')
+                }
+                return res
+            })
+    },
+    uploadUserImage (store, data) {
+        return ajax.postform('/chat/user/upload_head_img', data).then(res => {
+            if (res.success) {
+                alert(res.data.message)
+                store.commit('setUserImage', res.data.src)
+            }
+        })
+    }
+}
+
+var getters = {}
+
+export default {state, mutations, actions, getters}
