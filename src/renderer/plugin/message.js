e97e0ea1566de3be10d06fafd154793127d74299
ChenBaby <1911478180@qq.com>
2018-11-17 21:10:25 +0800

    封装message

--- src/renderer/plugin/message.js
+++ src/renderer/plugin/message.js
@@ -5,7 +5,7 @@ const MessageBox = Vue.extend(Message)
 
 let MyPlugin = {}
 MyPlugin.install = function (Vue) {
-    Vue.prototype.$message = Vue.prototype.$info = function (message, type = 'info') {
+    Vue.prototype.$message = Vue.prototype.$info = MyPlugin.$info = function (message, type = 'info') {
         let instance = new MessageBox({
             "data": { message, type }
         }).$mount()
@@ -17,15 +17,15 @@ MyPlugin.install = function (Vue) {
         })
     }
 
-    Vue.prototype.$warning = (message, type = 'warning') => {
+    Vue.prototype.$warning = MyPlugin.$warning = (message, type = 'warning') => {
         Vue.prototype.$message(message, type)
     }
 
-    Vue.prototype.$error = (message, type = 'error') => {
+    Vue.prototype.$error = MyPlugin.$error = (message, type = 'error') => {
         Vue.prototype.$message(message, type)
     }
 
-    Vue.prototype.$success = (message, type = 'success') => {
+    Vue.prototype.$success = MyPlugin.$success = (message, type = 'success') => {
         Vue.prototype.$message(message, type)
     }
 }
